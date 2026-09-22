9a70cf57495a8442c86efb4d84fafe2166494f36
Leonardo <lmontellano15@alumnos.uaq.mx>
2026-09-22 09:57:30 -0600

    Pruebas de funcionamiento

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,23 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Sistema de monitoreo");
         Tanque t1 = new Tanque("T-001", 50);
+        SensorNivel s1 = new SensorNivel("S-001", t1);
         System.out.println(t1.obtenerInformacion());
         t1.llenar(30);
+        s1.leerNivel();
+        System.out.println(s1.obtenerReporte());
         System.out.println(t1.obtenerInformacion());
         t1.vaciar(10);
+        s1.leerNivel();
+        System.out.println(s1.obtenerReporte());
         System.out.println(t1.obtenerInformacion());
         t1.llenar(50);
+        s1.leerNivel();
+        System.out.println(s1.obtenerReporte());
         System.out.println(t1.obtenerInformacion());
         t1.vaciar(70);
+        s1.leerNivel();
+        System.out.println(s1.obtenerReporte());
         System.out.println(t1.obtenerInformacion());
     }
 }
