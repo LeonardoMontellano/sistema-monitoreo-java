af92328829e6af51ccd190bda5f07402aaf334a6
aarontm08-rgb <aarontm08@gmail.com>
2026-09-21 19:30:14 -0600

    Agrega lectura y validacion del sensor

--- src/SensorNivel.java
+++ src/SensorNivel.java
@@ -16,4 +16,25 @@ public class SensorNivel {
     public double getUltimaLectura() {
         return this.ultimaLectura;
     }
+
+    public double leerNivel() {
+        if (this.tanqueAsociado != null) {
+            this.ultimaLectura = this.tanqueAsociado.getNivelActual();
+        }
+        return this.ultimaLectura;
+    }
+
+    public boolean esLecturaValida() {
+        if (this.tanqueAsociado == null) {
+            return false;
+        }
+        return this.ultimaLectura >= 0.0 && this.ultimaLectura <= this.tanqueAsociado.getCapacidadMaxima();
+    }
+
+    public String obtenerReporte() {
+        String estadoValidez = esLecturaValida() ? "VÁLIDA" : "INVÁLIDA";
+        return "Sensor: " + this.id +
+                " | Lectura: " + this.ultimaLectura + " L" +
+                " | Validación: " + estadoValidez;
+    }
 }
